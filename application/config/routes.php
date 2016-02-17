69503cfb9d4faf025fa009caf7ff20ea4e4434eb
Jamone Kelly <xg.monie089@gmail.com>
2016-02-16 20:42:00 -0500

    Add user routes for signup & signin

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,5 +50,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'welcome';
+// User routes
+$route['User_controller/signup'] = 'user/signup';
+$route['User_controller/signin'] = 'user/signin';
+// Event routes
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;

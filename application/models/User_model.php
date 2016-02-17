46514e93e8dcde554f2f112a748d4b9f1df37223
Jamone Kelly <xg.monie089@gmail.com>
2016-02-17 05:22:20 -0500

    Add validation logic and database creation for signup
    Check if the length of the supplied password is greater than or equal
    to 6 characters or else, stop execution.
    
    Also check if the supplied email is unique. If everything checks out,
    create the user.

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -39,7 +39,36 @@ class User_model extends CI_Model {
             return array('status' => false, 'msg' => 'Missing arguments');
         }
         
+        // Password length must be greater than 6 characters
+        if(strlen($password) < 6) {
+            return array('status' => false, 'msg' => 'Password is less than 6 characters.');
+        }
         
+        // Check if the email exists
+        $lowerCaseEmail = trim(strtolower($email));
+        $this->db->where('email', $lowerCaseEmail);
+        $query = $this->db->get('users');
+        
+        if($query->num_rows() > 0) {
+            // The email exists.. return
+            return array('status' => false, 'msg' => 'Email exists.');
+        }
+        
+        // Everything checks out.. Create the user 
+        // But first.. generate our `rememberToken` for users
+        $rememberToken = $this->generate(); // This token should be sent with all requests.. besides login/sign up
+        $insertArray = array(
+            'email'             => $lowerCaseEmail,
+            'password'          => hash('sha256', $password),
+            'remember_token'    => $rememberToken
+        );
+        
+        if($this->db->insert('users', $insertArray)) {
+            // Success
+            return array('status' => true, 'msg' => 'User created', 'remember_token' => $rememberToken);
+        } else {
+            return array('status' => false, 'msg' => 'Error creating user');
+        }
     }
     
     /**
@@ -53,6 +82,16 @@ class User_model extends CI_Model {
         if(empty($email) || empty($password)) {
             return array('status' => false, 'msg' => 'Missing arguments');
         }
+    }
+    
+    private function generate($length = 10) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
     }
 }
 ?>
